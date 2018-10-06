566385e778a5fc4457355c22c475162b7bab838d
Menna Anees <mennaanees193@gmail.com>
2018-10-06 19:51:28 +0200

    new changes

---
+++ server.js
@@ -0,0 +1,28 @@
+// const badCallback = (callback) => {
+//   setTimeout(() => {
+//      callback();
+//      callback();
+//   }, 1000);
+// }
+//
+//
+// badCallback(() => console.log('I want to run this only once'));
+
+const funcPromise = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('menna');
+      resolve();
+    }, 1000);
+  })
+}
+
+funcPromise()
+.then((res) => {
+  console.log('resolved', res);
+  throw new Error('erorrrrrrrrrr');
+})
+.then((res) => console.log('second', res))
+.then((res) => console.log('third'))
+.catch((err) => console.log('err///////////////', err))
+.then(() => console.log(')))))))))))))))))'))
